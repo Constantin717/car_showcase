3487fc1d12fecb26c706f16c00e128ce8c00b5a0
Constantin <constantinjorneac221@email.com>
2025-05-28 00:31:20 +0300

    Primul commit

---
+++ types/index.ts
@@ -0,0 +1,68 @@
+import { MouseEventHandler } from "react";
+
+export interface CarProps {
+  city_mpg: string;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: string;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
+
+export interface FilterProps {
+ manufacturer: string;
+    year?: number;
+    fuel?: string;
+    model: string;
+    limit: number;
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export interface CarCardProps {
+  model: string;
+  make: string;
+  mpg: number;
+  transmission: string;
+  year: number;
+  drive: string;
+  cityMPG: number;
+}
+
+export interface CustomButtonProps {
+   title: string;
+    containerStyles?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
+
+export interface SearchManuFacturerProps {
+  manufacturer: string;
+  setManuFacturer: (manufacturer: string) => void;
+}
---
+++ utils/index.ts
@@ -0,0 +1,67 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, model, year, fuel } = filters;
+
+  const headers = {
+    method: 'GET',
+    'X-RapidAPI-Key': 'e7afb030efmshdc797ec02e6c4f6p1c04c6jsncef19078e422',
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  // Construim URL-ul cu parametri dinamici
+  const url = new URL("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars");
+  
+  if (manufacturer) url.searchParams.append("make", manufacturer);
+  if (model) url.searchParams.append("model", model);
+  if (year) url.searchParams.append("year", year.toString());
+  if (fuel) url.searchParams.append("fuel_type", fuel);
+
+  const response = await fetch(url.toString(), {
+    headers: headers,
+  });
+
+  console.log("URL FETCH:", response.url);
+  console.log("RESPONSE STATUS:", response.status);
+  const result = await response.json();
+  console.log("RESULT:", result);
+
+  return result;
+}
+
+export const calculateCarRent = (year: number, cylinders: number, displacement: number) => {
+  const basePricePerDay = 50;
+  const cylinderFactor = 2.5;
+  const displacementFactor = 3.5;
+  const ageFactor = 0.1; 
+
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  const cylinderRate = cylinders * cylinderFactor;
+  const displacementRate = displacement * displacementFactor;
+
+  const rentalRatePerDay = basePricePerDay + cylinderRate + displacementRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append("customer", "img");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  if (angle) url.searchParams.append("angle", angle);
+
+  return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  return `${window.location.pathname}?${searchParams.toString()}`;
+}
